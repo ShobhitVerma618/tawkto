93edf2d5213d7e8d7869fac9e76120053af61465
ShobhitVerma618 <103369695+ShobhitVerma618@users.noreply.github.com>
2023-09-26 15:42:01 +0530

    Update tawkto.js

--- tawkto.js
+++ tawkto.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const fetch = require('fetch');
 const crypto = require('crypto');
 
 const app = express();
